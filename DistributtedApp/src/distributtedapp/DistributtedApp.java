df42fc74c355c5080f0a1856004575141d86d2a3
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-05-09 16:46:21 +0300

    Refactored package name

--- DistributtedApp/src/distributtedapp/DistributtedApp.java
+++ DistributtedApp/src/distributtedapp/DistributtedApp.java
@@ -5,7 +5,7 @@
  */
 package distributtedapp;
 
-import distributtedapp.databsemanager.MySQLJDBCUtil;
+import distributtedapp.databasemanager.MySQLJDBCUtil;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
