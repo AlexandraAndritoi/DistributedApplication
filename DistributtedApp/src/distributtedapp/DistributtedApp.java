c3032d7d01fcf15a32dbec5dab3c52b1c40e50ff
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-05-09 16:11:47 +0300

    Connected to database

---
+++ DistributtedApp/src/distributtedapp/DistributtedApp.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributtedapp;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Alexandra
+ */
+public class DistributtedApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try (Connection conn = MySQLJDBCUtil.getConnection();
+               Statement stmt = conn.createStatement()) {
+            
+            System.out.println(String.format("Connected to database %s "
+                    + "successfully.", conn.getCatalog()));
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
---
+++ DistributtedApp/src/distributtedapp/MySQLJDBCUtil.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributtedapp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Alexandra
+ */
+public class MySQLJDBCUtil {
+    public static Connection getConnection() throws SQLException
+    {
+        Connection conn = null;
+        
+        //System.out.println("mysqljdbc.MySQLJDBCUtil.getConnection()" + "\nAJUNGE AICI");
+        
+        
+        
+        try(FileInputStream f = new FileInputStream("db.properties"))
+        {
+            
+            //load the properties file containg the database properties
+            Properties pros = new Properties();
+            pros.load(f);
+            
+            //assing the database parameters
+            String url = pros.getProperty("url");
+            String user = pros.getProperty("user");
+            String password = pros.getProperty("password");
+            
+            //System.out.println("mysqljdbc.MySQLJDBCUtil.getConnection()\n" + url + "\n" + user + "\n" + password);
+            
+            //create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+        }catch(IOException e)
+        {
+            System.out.println("mysqljdbc.MySQLJDBCUtil.getConnection() " + e.getMessage());
+        }
+        
+        return conn;
+    }
+}
