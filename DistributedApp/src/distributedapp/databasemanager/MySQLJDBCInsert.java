247fda2f3587fa1b15d3d462be9fd04059db5762
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-05-11 23:56:39 +0300

    Inserting a user into the database successfully

--- DistributedApp/src/distributedapp/databasemanager/MySQLJDBCInsert.java
+++ DistributedApp/src/distributedapp/databasemanager/MySQLJDBCInsert.java
@@ -7,9 +7,12 @@ package distributedapp.databasemanager;
 
 import distributedapp.servermanager.interfaces.User;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 /**
  *
@@ -27,14 +30,33 @@ public class MySQLJDBCInsert {
                 + "Serializable object is ready to be inserted...");
         System.out.println("distributedapp.databasemanager.MySQLJDBCInsert.insertUser() "
                 + "user's address: " + user.getAddress());
-/*
+        
+        ResultSet rs = null;
         int userId = 0;
+        
         String sql = "INSERT INTO user(username,first_name,last_name,email,password,"
-                + "number,adress,country,zipcode,birthdate)"
+                + "number,address,country,zipcode,birth)"
                 + " VALUES(?,?,?,?,?,?,?,?,?,?)";
         
         try(Connection conn = MySQLJDBCUtil.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);){
+            pstmt.setString(1,user.getUsername());
+            pstmt.setString(2, user.getFirstName());
+            pstmt.setString(3, user.getLastName());
+            pstmt.setString(4, user.getEmail());
+            pstmt.setString(5, user.getPassword());
+            pstmt.setString(6, user.getPhone());
+            pstmt.setString(7, user.getAddress());
+            pstmt.setString(8, user.getCountry());
+            pstmt.setString(9, user.getZipcode());
+            pstmt.setDate(10, Date.valueOf(user.getBirthdate()));
+            
+            int rowAffected = pstmt.executeUpdate();
+            if(rowAffected == 1){
+                rs = pstmt.getGeneratedKeys();
+                if(rs.next())
+                    userId = rs.getInt(1);
+            }
             
         } catch(SQLException e){
             System.out.println("distributedapp.databasemanager.MySQLJDBCInsert.insertUser(): "
@@ -47,7 +69,13 @@ public class MySQLJDBCInsert {
                         + e.getMessage());
             }
         }
-*/
+        
+        if(userId != 0) 
+            System.out.println("distributedapp.databasemanager.MySQLJDBCInsert.insertUser(): "
+                + "New user ID: " + userId);
+        else System.out.println("distributedapp.databasemanager.MySQLJDBCInsert.insertUser(): "
+                + "Error inserting new user!");
+        
     }
     
     public static MySQLJDBCInsert getMySQLJDBCInsert(){
